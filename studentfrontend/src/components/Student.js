f3593769b41d6fba787a887a191b0bebd61d0a06
Abdessamad-Jadid <ajadid@miu.edu>
2022-03-27 17:58:35 -0500

    add @CrossOrigin to the student controller

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -17,6 +17,15 @@ const handleClick=(e)=>
     e.preventDefault()
     const student={name,address}
     console.log(student)
+    fetch("http://localhost:8080/student/add",{
+
+        method:"POST",
+        headers:{"Content-Type" : "application/json"},
+        body:JSON.stringfy(student)
+
+    }).then(()=>{
+        console.log("New student added")
+    })
 }
 
 
