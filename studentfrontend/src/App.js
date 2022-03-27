6379e18c7b65f643a8fd6cb0fde78dbf0d3e0e74
Abdessamad-Jadid <ajadid@miu.edu>
2022-03-27 17:15:39 -0500

    Add Student.js component

--- studentfrontend/src/App.js
+++ studentfrontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Appbar from './components/Appbar';
+import Student from './components/Student';
 
 function App() {
   return (
     <div className="App">
       <Appbar />
+      <Student />
     </div>
   );
 }
