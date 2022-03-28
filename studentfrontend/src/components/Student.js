8ec5fe13e26a061784ec04e00834efee379307ee
Abdessamad-Jadid <ajadid@miu.edu>
2022-03-28 00:37:13 -0500

    fix Student.js component

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -1,57 +1,83 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { Paper } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Container ,Paper,Button} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+     
+    },
+  },
+}));
 
 export default function Student() {
+    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
+    const[name,setName]=useState('')
+    const[address,setAddress]=useState('')
+    const[students,setStudents]=useState([])
+    const classes = useStyles();
 
-const paperStyle ={padding:'50px 20px', width:600,margin:"20px auto"}
-const[name,setName] = React.useState('')
-const [address,setAddress] = React.useState('');
-//const classes = React.useStyles('');
-
-
-const handleClick=(e)=>
-{
+  const handleClick=(e)=>{
     e.preventDefault()
     const student={name,address}
     console.log(student)
     fetch("http://localhost:8080/student/add",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(student)
 
-        method:"POST",
-        headers:{"Content-Type" : "application/json"},
-        body:JSON.stringfy(student)
-
-    }).then(()=>{
-        console.log("New student added")
-    })
+  }).then(()=>{
+    console.log("New Student added")
+  })
 }
 
-
+useEffect(()=>{
+  fetch("http://localhost:8080/student/getallstudents")
+  .then(res=>res.json())
+  .then((result)=>{
+    setStudents(result);
+  }
+)
+},[])
   return (
-      <Paper elevation={3} style={paperStyle}>
-        <h1>Add Student</h1>
 
-            <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-            <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth
-            value={name}
-            onChange={(e)=>setName(e.target.value)}
-            />
-            <TextField id="outlined-basic" label="Student Address" variant="outlined" fullWidth
-            value={address}
-            onChange={(e)=>setAddress(e.target.value)}
-            />
-            <Button variant="contained" onClick={handleClick}>Submit</Button>
-            </Box>
-        
+    <Container>
+        <Paper elevation={3} style={paperStyle}>
+            <h1>Add Student</h1>
+
+    <form className={classes.root} noValidate autoComplete="off">
+    
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth 
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      />
+      <TextField id="outlined-basic" label="Student Adress" variant="outlined" fullWidth
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}
+      />
+      <Button variant="contained" color="secondary" onClick={handleClick}>
+  Submit
+</Button>
+    </form>
+   
     </Paper>
+    <h1>Students</h1>
+
+    <Paper elevation={3} style={paperStyle}>
+
+      {students.map(student=>(
+        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
+         Id:{student.id}<br/>
+         Name:{student.name}<br/>
+         Address:{student.address}
+
+        </Paper>
+      ))
+}
+    </Paper>
+
+    </Container>
   );
 }
